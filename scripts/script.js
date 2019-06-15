7742dd297adf1ac51e89761ae0865e254c6b62fa
Nikhil Singh <nikhilsingh498@gmail.com>
2019-06-15 08:17:11 +0530

    Update timeline and dwnld on mobile view

--- scripts/script.js
+++ scripts/script.js
@@ -7,9 +7,7 @@ function colorAnimate(event, color, blocknAnimate){
     document.getElementById("diag").style.animation = "diagonal 0.3s linear";
     document.getElementById("diag").style.animationFillMode = "forwards";
     navId.forEach( element => {
-        console.log(event.target.classList)
         if (element == event.target.classList[1]){
-            console.log(blocknAnimate.slice(0,-4));
             let ele = document.getElementsByClassName(element);
             for(let i=0; i< ele.length; i++){
                 ele[i].style.color = colors[color];
@@ -28,7 +26,6 @@ function colorAnimate(event, color, blocknAnimate){
             document.getElementById(element).style.display = "block"
         }
     })
-    console.log(blocknAnimate.slice(0,-4));
     document.getElementById(blocknAnimate.slice(0,-4) + "-content").classList.add("animated" , "fadeInUp", "faster");
     document.getElementById(blocknAnimate.slice(0,-4) + "-img").classList.add("animated" ,"fadeInRight", "faster");
 }
@@ -87,7 +84,6 @@ function smColorUpdate(e, int) {
         }
     })
     smMainPageId.forEach( ele => {
-        console.log(e.target.id + "-top")
         if(ele == e.target.id + "-top"){
             document.getElementById(ele).style.display = "block";
             document.getElementById(ele).classList.add("animated" , "fadeInUp", "faster");
