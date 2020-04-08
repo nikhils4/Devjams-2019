59506017100850dbb51bd39aa8bd922cb9c73159
Nikhil Singh <nikhilsingh498@gmail.com>
2020-04-09 02:36:59 +0530

    Revert few changes

--- scripts/script.js
+++ scripts/script.js
@@ -31,21 +31,21 @@ function colorAnimate(event, color, blocknAnimate){
     document.getElementById(blocknAnimate.slice(0,-4) + "-img").classList.add("animated" ,"fadeInRight", "faster");
 }
 
-// document.getElementById("back-btn").addEventListener("click", (e) => {
-//     // document.getElementById("previous-events-top").style.display = "none";
-//     document.getElementById("home-top").style.display = "block";
-//     document.getElementById("home-top").classList.add("animated" ,"fadeInUp", "faster");
-//     document.getElementById("img-top").style.display = "block";
-// } )
+document.getElementById("back-btn").addEventListener("click", (e) => {
+    document.getElementById("previous-events-top").style.display = "none";
+    document.getElementById("home-top").style.display = "block";
+    document.getElementById("home-top").classList.add("animated" ,"fadeInUp", "faster");
+    document.getElementById("img-top").style.display = "block";
+} )
 
-// document.getElementsByClassName("nav-box-right-heading")[0].addEventListener("click", (e) => {
-//     mainPageId.forEach( element => {
-//         document.getElementById(element).style.display = "none"
-//     })
-//     document.getElementById("img-top").style.display = "none";
-//     // document.getElementById("previous-events-top").style.display = "block";
-//     // document.getElementById("previous-events-top").classList.add("animated" ,"fadeInRight", "faster");
-// } )
+document.getElementsByClassName("nav-box-right-heading")[0].addEventListener("click", (e) => {
+    mainPageId.forEach( element => {
+        document.getElementById(element).style.display = "none"
+    })
+    document.getElementById("img-top").style.display = "none";
+    document.getElementById("previous-events-top").style.display = "block";
+    document.getElementById("previous-events-top").classList.add("animated" ,"fadeInRight", "faster");
+} )
 
 
 
@@ -59,24 +59,22 @@ function navAnimate() {
     document.getElementById("sm-main").style.animationFillMode = "forwards";
     document.getElementById("sm-diag").style.animation = "sm-diagonal-logo 0.4s linear";
     document.getElementById("sm-diag").style.animationFillMode = "forwards";
-    document.getElementById("sm-main").style.paddingLeft = "50px";
+    document.getElementById("sm-main").style.paddingLeft = "20px";
 }
 
-// function navAnimateGeneral() {
-//     document.getElementById("nav-top").style.animation = "sm-navbar-animate 0.4s linear";
-//     document.getElementById("nav-top").style.animationFillMode = "forwards";
-//     document.getElementById("sm-about-us").style.animation = "sm-aboutUs-animate 0.4s linear";
-//     document.getElementById("sm-about-us").style.animationFillMode = "forwards";
-//     document.getElementById("sm-diag").style.animation = "sm-diagonal-logo 0.4s linear";
-//     document.getElementById("sm-diag").style.animationFillMode = "forwards";
-//     document.getElementById("sm-about-us").style.paddingLeft = "150px";
-// }
+function navAnimateGeneral() {
+    document.getElementById("nav-top").style.animation = "sm-navbar-animate 0.4s linear";
+    document.getElementById("nav-top").style.animationFillMode = "forwards";
+    document.getElementById("sm-about-us").style.animation = "sm-aboutUs-animate 0.4s linear";
+    document.getElementById("sm-about-us").style.animationFillMode = "forwards";
+    document.getElementById("sm-diag").style.animation = "sm-diagonal-logo 0.4s linear";
+    document.getElementById("sm-diag").style.animationFillMode = "forwards";
+    document.getElementById("sm-about-us").style.paddingLeft = "150px";
+}
 
-let smnavId = ["sm-about-us", "sm-hackathon", "sm-downloads", "sm-speakers", "sm-sponsors",  "sm-timeline", "sm-contact-us", "sm-main"]
-// "sm-prev-events",
-let smMainPageId = [ "sm-about-us-top", "sm-hackathon-top", "sm-timeline-top", "sm-speakers-top", "sm-sponsors-top", "sm-contact-us-top", "sm-downloads-top", "sm-main-top"]
+let smnavId = ["sm-about-us", "sm-hackathon", "sm-downloads", "sm-speakers", "sm-sponsors",  "sm-timeline", "sm-contact-us", "sm-main", "sm-prev-events"]
+let smMainPageId = [ "sm-about-us-top", "sm-hackathon-top", "sm-timeline-top", "sm-speakers-top", "sm-sponsors-top", "sm-contact-us-top", "sm-downloads-top", "sm-main-top", "sm-prev-events-top"]
 
-// "sm-prev-events-top"
 function smColorUpdate(e, int) {
     smnavId.forEach(element => {
         if (element == e.target.id) {
